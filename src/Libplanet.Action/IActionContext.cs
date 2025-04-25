35569ec95e7692afb932dfbd68f44d66dee530dd
s2quake <han0210@netsgo.com>
2025-04-25 22:14:28 +0900

    wip

--- src/Libplanet.Action/IActionContext.cs
+++ src/Libplanet.Action/IActionContext.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.Contracts;
 using Libplanet.Action.State;
 using Libplanet.Crypto;
 using Libplanet.Types.Assets;
@@ -6,120 +5,33 @@ using Libplanet.Types.Blocks;
 using Libplanet.Types.Evidence;
 using Libplanet.Types.Tx;
 
-namespace Libplanet.Action
+namespace Libplanet.Action;
+
+public interface IActionContext
 {
-    /// <summary>
-    /// Contextual data determined by a transaction and a block.
-    /// Passed to <see cref="IAction.Execute(IActionContext)"/> method.
-    /// </summary>
-    public interface IActionContext
-    {
-        /// <summary>
-        /// The <see cref="Transaction.Signer"/> of the <see cref="Transaction"/> that contains
-        /// the <see cref="IAction"/> to be executed.  If the <see cref="IAction"/> is
-        /// not part of a <see cref="Transaction"/>, e.g. <see cref="IBlockPolicy.BlockAction"/>,
-        /// this is set to <see cref="Block.Miner"/> instead.
-        /// </summary>
-        [Pure]
-        Address Signer { get; }
+    Address Signer { get; }
 
-        /// <summary>
-        /// The <see cref="Transaction.Id"/> of the <see cref="Transaction"/> that contains
-        /// the <see cref="IAction"/>.  If the <see cref="IAction"/> is not part of
-        /// a <see cref="Transaction"/>, e.g. <see cref="IBlockPolicy.BlockAction"/>,
-        /// this is set to <see langword="null"/>.
-        /// </summary>
-        [Pure]
-        TxId? TxId { get; }
+    TxId? TxId { get; }
 
-        /// <summary>
-        /// The <see cref="Block.Miner"/> of the <see cref="Block"/> that contains
-        /// the <see cref="IAction"/>.
-        /// </summary>
-        [Pure]
-        Address Miner { get; }
+    Address Miner { get; }
 
-        /// <summary>
-        /// The <see cref="Block.Index"/> of the <see cref="Block"/> that contains
-        /// the <see cref="IAction"/>.
-        /// </summary>
-        [Pure]
-        long BlockHeight { get; }
+    long BlockHeight { get; }
 
-        /// <summary>
-        /// The <see cref="Block.ProtocolVersion"/> of the <see cref="Block"/> that contains
-        /// the <see cref="IAction"/>.
-        /// </summary>
-        [Pure]
-        int BlockProtocolVersion { get; }
+    int BlockProtocolVersion { get; }
 
-        /// <summary>
-        /// The <see cref="BlockCommit"/> about previous <see cref="Block"/>'s vote information.
-        /// <see langword="null"/> if the block is the genesis block.
-        /// </summary>
-        [Pure]
-        BlockCommit? LastCommit { get; }
+    BlockCommit? LastCommit { get; }
 
-        /// <summary>
-        /// A null delta of states, which means it represents the states
-        /// before <see cref="IAction"/> executes.
-        /// <para>Although a <see cref="IAccount"/> instance is
-        /// immutable, it has several manipulative methods that returns
-        /// new <see cref="IAccount"/> instances with some "dirty"
-        /// states.  These kinds of dirty <see cref="IWorld"/>
-        /// instances can be returned by <see
-        /// cref="IAction.Execute(IActionContext)"/> method.</para>
-        /// </summary>
-        [Pure]
-        IWorld PreviousState { get; }
+    IWorld PreviousState { get; }
 
-        /// <summary>
-        /// The random seed to use for pseudorandom number generator.  This value
-        /// is determined by various block properties, the signature of the transaction
-        /// containing the action to execute, and index of the action to execute, which is
-        /// deterministic so that every node can replay the same action and
-        /// then reproduce the same result, while neither a single block miner
-        /// nor a single transaction signer can predict the result and cheat.
-        /// </summary>
-        /// <seealso cref="GetRandom"/>
-        int RandomSeed { get; }
+    int RandomSeed { get; }
 
-        /// <summary>
-        /// Whether this action is executed as a policy action.
-        /// <see langword="false"/> if it is a user action.
-        /// </summary>
-        [Pure]
-        bool IsPolicyAction { get; }
+    bool IsPolicyAction { get; }
 
-        /// <summary>
-        /// Max gas price set by the transaction.
-        /// <see langword="null"/> if the action does not belongs to a transaction.
-        /// </summary>
-        [Pure]
-        FungibleAssetValue? MaxGasPrice { get; }
+    FungibleAssetValue? MaxGasPrice { get; }
 
-        /// <summary>
-        /// A list of <see cref="Transaction"/>s that are included in a <see cref="Block"/> as
-        /// the <see cref="IAction"/> to be evaluated.  This information is provided only if
-        /// <see cref="IsPolicyAction"/> is <see langword="true"/>, otherwise returns an empty set.
-        /// </summary>
-        [Pure]
-        IReadOnlyList<Transaction> Txs { get; }
+    ImmutableSortedSet<Transaction> Txs { get; }
 
-        /// <summary>
-        /// A list of <see cref="EvidenceBase"/>s that are included in a
-        /// <see cref="Block"/>.
-        /// </summary>
-        [Pure]
-        IReadOnlyList<EvidenceBase> Evidence { get; }
+    ImmutableSortedSet<EvidenceBase> Evidence { get; }
 
-        /// <summary>
-        /// Returns a newly initialized <see cref="IRandom"/> using <see cref="RandomSeed"/>
-        /// as its seed value.
-        /// </summary>
-        /// <returns>A newly initialized <see cref="IRandom"/> using <see cref="RandomSeed"/>
-        /// as its seed value.</returns>
-        [Pure]
-        IRandom GetRandom();
-    }
+    IRandom GetRandom();
 }
--- src/Libplanet.Action/IActionEvaluation.cs
+++ src/Libplanet.Action/IActionEvaluation.cs
@@ -1,33 +1,15 @@
 using Bencodex.Types;
 using Libplanet.Action.State;
 
-namespace Libplanet.Action
+namespace Libplanet.Action;
+
+public interface IActionEvaluation
 {
-    public interface IActionEvaluation
-    {
-        /// <summary>
-        /// An action data to evaluate. When the
-        /// <see cref="InputContext"/>.<see cref="IActionContext.IsPolicyAction"/> is true,
-        /// use <see cref="IBlockPolicy.BlockAction"/> instead of trying deserialization.
-        /// </summary>
-        public IValue Action { get; }
+    public IValue Action { get; }
 
-        /// <summary>
-        /// An input <see cref="IActionContext"/> to evaluate
-        /// <see cref="Action"/>.
-        /// </summary>
-        /// <remarks>Its <see cref="IActionContext.Random"/> property
-        /// is not consumed yet.</remarks>
-        public IActionContext InputContext { get; }
+    public IActionContext InputContext { get; }
 
-        /// <summary>
-        /// The result states that <see cref="Action"/> makes.
-        /// </summary>
-        public IWorld OutputState { get; }
+    public IWorld OutputState { get; }
 
-        /// <summary>
-        /// An exception that had risen during evaluation.
-        /// </summary>
-        public Exception? Exception { get; }
-    }
+    public Exception? Exception { get; }
 }
