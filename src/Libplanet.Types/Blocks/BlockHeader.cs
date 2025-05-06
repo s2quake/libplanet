770e17317d58e8b1404b61490b149cd2aea03715
s2quake <han0210@netsgo.com>
2025-05-07 08:17:26 +0900

    wip

--- src/Libplanet.Types/Blocks/BlockHeader.cs
+++ src/Libplanet.Types/Blocks/BlockHeader.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography;
+using Bencodex.Types;
 using Libplanet.Serialization;
 using Libplanet.Types.Crypto;
 
@@ -7,43 +8,65 @@ namespace Libplanet.Types.Blocks;
 [Model(Version = 1)]
 public sealed record class BlockHeader
 {
+    public const int CurrentProtocolVersion = 0;
+
+    private static readonly TimeSpan TimestampThreshold = TimeSpan.FromSeconds(15);
+
     public static BlockHeader Empty { get; } = new();
 
     [Property(0)]
-    public HashDigest<SHA256> StateRootHash { get; init; }
+    public int ProtocolVersion { get; init; } = CurrentProtocolVersion;
 
     [Property(1)]
-    public ImmutableArray<byte> Signature { get; init; }
-
-    [Property(2)]
-    public BlockHash BlockHash { get; init; }
-
-    [Property(3)]
-    public int ProtocolVersion { get; init; } = BlockMetadata.CurrentProtocolVersion;
-
-    [Property(4)]
     public long Height { get; init; }
 
-    [Property(5)]
+    [Property(2)]
     public DateTimeOffset Timestamp { get; init; }
 
-    [Property(6)]
+    [Property(3)]
     public Address Proposer { get; init; }
 
-    [Property(7)]
+    [Property(4)]
     public BlockHash PreviousHash { get; init; }
 
-    [Property(8)]
+    [Property(5)]
     public BlockCommit LastCommit { get; init; } = BlockCommit.Empty;
 
-    [Property(9)]
-    public HashDigest<SHA256> RawHash { get; init; }
+    public static ImmutableArray<byte> MakeSignature(PrivateKey privateKey, HashDigest<SHA256> stateRootHash)
+    {
+        var msg = ModelSerializer.SerializeToBytes(stateRootHash);
+        var sig = privateKey.Sign(msg);
+        return ImmutableArray.Create(sig);
+    }
 
-    [Property(10)]
-    public HashDigest<SHA256> TxHash { get; init; }
+    public void ValidateTimestamp() => ValidateTimestamp(DateTimeOffset.UtcNow);
 
-    [Property(11)]
-    public HashDigest<SHA256> EvidenceHash { get; init; }
+    public void ValidateTimestamp(DateTimeOffset currentTime)
+    {
+        if (currentTime + TimestampThreshold < Timestamp)
+        {
+            var message = $"The block #{Height}'s timestamp " +
+                $"({Timestamp}) is later than now " +
+                $"({currentTime}, threshold: {TimestampThreshold}).";
+            throw new InvalidOperationException(message);
+        }
+    }
 
-    public override string ToString() => $"#{Height} {BlockHash}";
+    public BlockHash DeriveBlockHash(in HashDigest<SHA256> stateRootHash, in ImmutableArray<byte> signature)
+    {
+        var list = new List(
+            ModelSerializer.Serialize(this),
+            ModelSerializer.Serialize(stateRootHash),
+            ModelSerializer.Serialize(signature));
+        return BlockHash.DeriveFrom(BencodexUtility.Encode(list));
+    }
+
+    public bool VerifySignature(ImmutableArray<byte> signature, HashDigest<SHA256> stateRootHash)
+    {
+        var message = ModelSerializer.SerializeToBytes(stateRootHash).ToImmutableArray();
+        return PublicKey.Verify(Proposer, message, signature);
+    }
+
+    public HashDigest<SHA256> DerivePreEvaluationHash()
+        => HashDigest<SHA256>.DeriveFrom(ModelSerializer.SerializeToBytes(this));
 }
