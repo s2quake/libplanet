de5a925cc6e05e642ab83e626adab143a6133a41
s2quake <han0210@netsgo.com>
2025-06-15 10:04:11 +0900

    wip

--- src/Libplanet.Net/Messages/DifferentVersionMessage.cs
+++ src/Libplanet.Net/Messages/DifferentVersionMessage.cs
@@ -1,5 +1,6 @@
 namespace Libplanet.Net.Messages;
 
+[Obsolete]
 public sealed record class DifferentVersionMessage : MessageBase
 {
     public override MessageType Type => MessageType.DifferentVersion;
