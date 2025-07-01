edd3189b4d7b267bdc61af1d27e3bba5285e1b6e
s2quake <han0210@netsgo.com>
2025-07-01 18:44:36 +0900

    wip

--- test/Libplanet.Net.Tests/Consensus/ConsensusExtensions.cs
+++ test/Libplanet.Net.Tests/Consensus/ConsensusExtensions.cs
@@ -1,3 +1,6 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Libplanet.Net.Consensus;
 using Libplanet.Net.Messages;
 
 namespace Libplanet.Net.Tests.Consensus;
@@ -17,6 +20,24 @@ public static class ConsensusExtensions
         else if (consensusMessage is ConsensusProposalMessage proposalMessage)
         {
             @this.Post(proposalMessage.Proposal);
+        }
+    }
+
+    public static async Task WaitUntilAsync(
+        this Net.Consensus.Consensus @this, int round, ConsensusStep step, CancellationToken cancellationToken)
+    {
+        using var resetEvent = new ManualResetEvent(false);
+        using var _ = @this.StepChanged.Subscribe(e =>
+        {
+            if (@this.Round == round && @this.Step == step)
+            {
+                resetEvent.Set();
+            }
+        });
+
+        while (!resetEvent.WaitOne(0))
+        {
+            await Task.Delay(100, cancellationToken);
         }
     }
 }
