36fc798041cd56eb319be933a00770e23c2ee32e
s2quake <han0210@netsgo.com>
2025-07-28 15:42:24 +0900

    wip

--- src/Libplanet.Net/MessageHandlerScope.cs
+++
@@ -1,7 +0,0 @@
-// namespace Libplanet.Net;
-
-// public sealed class MessageHandlerScope(MessageRouter handlers, IMessageHandler handler)
-//     : IDisposable
-// {
-//     void IDisposable.Dispose() => handlers.Unregister(handler);
-// }
