ccb7f857a9b9db831af86f934ea8688a2f98e96c
s2quake <han0210@netsgo.com>
2025-08-28 15:38:14 +0900

    wip

--- src/Libplanet/Extensions/BlockchainExtensions.cs
+++ src/Libplanet/Extensions/BlockchainExtensions.cs
@@ -1,5 +1,6 @@
-using Libplanet.Types;
+using System.Security.Cryptography;
 using Libplanet.State;
+using Libplanet.Types;
 using static Libplanet.State.SystemAddresses;
 
 namespace Libplanet.Extensions;
@@ -12,12 +13,51 @@ public static class BlockchainExtensions
         return @this.GetWorld(stateRootHash).GetValidators();
     }
 
-    public static object GetValue(this Blockchain @this, Address address, string key)
-        => @this.GetWorld().GetAccount(address).GetValue(key);
+    public static object GetValue(this Blockchain @this, string name, string key)
+        => @this.GetWorld().GetAccount(name).GetValue(key);
+
+    public static object GetValue(this Blockchain @this, Address name, Address key)
+        => @this.GetWorld().GetAccount(name).GetValue(key);
+
+    public static object GetValue(this Blockchain @this, int height, string name, string key)
+        => @this.GetWorld(height).GetAccount(name).GetValue(key);
+
+    public static object GetValue(this Blockchain @this, int height, Address name, Address key)
+        => @this.GetWorld(height).GetAccount(name).GetValue(key);
+
+    public static object GetValue(this Blockchain @this, BlockHash blockHash, string name, string key)
+        => @this.GetWorld(blockHash).GetAccount(name).GetValue(key);
+
+    public static object GetValue(this Blockchain @this, BlockHash blockHash, Address name, Address key)
+        => @this.GetWorld(blockHash).GetAccount(name).GetValue(key);
+
+    public static object GetValue(this Blockchain @this, HashDigest<SHA256> stateRootHash, string name, string key)
+        => @this.GetWorld(stateRootHash).GetAccount(name).GetValue(key);
+
+    public static object GetValue(this Blockchain @this, HashDigest<SHA256> stateRootHash, Address name, Address key)
+        => @this.GetWorld(stateRootHash).GetAccount(name).GetValue(key);
 
     public static object GetSystemValue(this Blockchain @this, string key)
         => @this.GetWorld().GetAccount(SystemAccount).GetValue(key);
-    
+
     public static object GetSystemValue(this Blockchain @this, Address key)
         => @this.GetWorld().GetAccount(SystemAccount).GetValue(key);
+
+    public static object GetSystemValue(this Blockchain @this, int height, string key)
+        => @this.GetWorld(height).GetAccount(SystemAccount).GetValue(key);
+
+    public static object GetSystemValue(this Blockchain @this, int height, Address key)
+        => @this.GetWorld(height).GetAccount(SystemAccount).GetValue(key);
+
+    public static object GetSystemValue(this Blockchain @this, BlockHash blockHash, string key)
+        => @this.GetWorld(blockHash).GetAccount(SystemAccount).GetValue(key);
+
+    public static object GetSystemValue(this Blockchain @this, BlockHash blockHash, Address key)
+        => @this.GetWorld(blockHash).GetAccount(SystemAccount).GetValue(key);
+
+    public static object GetSystemValue(this Blockchain @this, HashDigest<SHA256> stateRootHash, string key)
+        => @this.GetWorld(stateRootHash).GetAccount(SystemAccount).GetValue(key);
+
+    public static object GetSystemValue(this Blockchain @this, HashDigest<SHA256> stateRootHash, Address key)
+        => @this.GetWorld(stateRootHash).GetAccount(SystemAccount).GetValue(key);
 }
