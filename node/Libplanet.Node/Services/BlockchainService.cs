7d8987f02599c59c8c8742add4672f12ac651b42
s2quake <han0210@netsgo.com>
2025-10-15 10:56:04 +0900

    wip

--- node/Libplanet.Node/Services/BlockchainService.cs
+++ node/Libplanet.Node/Services/BlockchainService.cs
@@ -1,20 +1,35 @@
+using Libplanet.Node.Options;
 using Libplanet.Types;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Libplanet.Node.Services;
 
 internal sealed class BlockchainService : IBlockchainService
 {
     public BlockchainService(
+        IOptions<NodeOptions> nodeOptions,
         RepositoryService repositoryService,
         ILoggerFactory loggerFactory)
     {
-        Blockchain = new Blockchain(
-            repositoryService.Repository,
-            new BlockchainOptions
+        var repository = repositoryService.Repository;
+        var options = new BlockchainOptions
+        {
+            Logger = loggerFactory.CreateLogger<Blockchain>(),
+        };
+        if (repositoryService.Type == RepositoryType.Memory)
+        {
+            var signer = PrivateKey.Parse(nodeOptions.Value.PrivateKey).AsSigner();
+            var genesisBlock = new GenesisBlockBuilder
             {
-                Logger = loggerFactory.CreateLogger<Blockchain>(),
-            });
+                Validators = [new Validator { Address = signer.Address }],
+            }.Create(signer);
+            Blockchain = new Blockchain(genesisBlock, repository, options);
+        }
+        else
+        {
+            Blockchain = new Blockchain(repository, options);
+        }
     }
 
     public Blockchain Blockchain { get; }
